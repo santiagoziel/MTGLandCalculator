3440e3320c75be319009b758a2efbff3adcdc94c
santiagoziel <santiago_oziel92@hotmail.com>
2022-06-27 16:58:33 -0500

    added util to get mtg goldfish decks using bs4

--- utils/goldfishGetDecks.py
+++ utils/goldfishGetDecks.py
@@ -1,23 +1,34 @@
-import requests
+'''
+gets a list of mtg goldfish decks that coointain certain cards
+'''
 
-url = "https://www.mtggoldfish.com/deck_searches/create?utf8=✓"
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.mtggoldfish.com/deck_searches/create?"
 payload = {
+"utf8" : "✓",
 "deck_search[name]" : "",
 "deck_search[format]" : "pioneer",
-"deck_search[types][]": "tournament, user",
+"deck_search[types][]": "tournament",
 "deck_search[player]":"",
 "deck_search[date_range]":"06/13/2022+-+06/27/2022",
-"deck_search[deck_search_card_filters_attributes][0][card]":"Young+Pyromancer",
+"counter":"7",
+"commit":"Search",
+"deck_search[deck_search_card_filters_attributes][0][card]":"Elvish+Mystic",
 "deck_search[deck_search_card_filters_attributes][0][quantity]":"4",
 "deck_search[deck_search_card_filters_attributes][0][type]":"maindeck",
-"deck_search[deck_search_card_filters_attributes][1][card]":"",
-"deck_search[deck_search_card_filters_attributes][1][quantity]":"1",
-"deck_search[deck_search_card_filters_attributes][1][type]":"maindeck",
-"counter":"2",
-"commit":"Search"
+"deck_search[deck_search_card_filters_attributes][2][card]":"Llanowar+Elves",
+"deck_search[deck_search_card_filters_attributes][2][quantity]":"4",
+"deck_search[deck_search_card_filters_attributes][2][type]":"maindeck"
 }
 payload_str = "&".join("%s=%s" % (k,v) for k,v in payload.items())
 r = requests.get(url, params=payload_str)
 
-with open("response.html", "w") as f:
-    f.write(r.text)
+soup = BeautifulSoup(r.text, 'html.parser')
+table = soup.find("table", {"class": "table-striped"})
+rows =  table.find_all("tr")
+for row in rows:
+    a = row.find("a")
+    if a != None:
+        counter += 1
+        print(a['href'])
