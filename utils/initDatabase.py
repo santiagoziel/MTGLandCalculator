941d5279e05e4e176b4ca7f0af834fd4cfc4b71b
santiagoziel <santiago_oziel92@hotmail.com>
2022-06-29 12:52:32 -0500

    formated init database

--- utils/initDatabase.py
+++ utils/initDatabase.py
@@ -4,14 +4,15 @@ import json
 with open('cards\sampleAtomic.json', encoding="UTF-8") as f:
     data = json.load(f)
     for card in data["data"]:
-        print("-----------")
-        print(f"Cheking {card}")
         for face in data["data"][f"{card}"]:
-            if not "Land" in face["type"]:
-                continue
             if len(data["data"][f"{card}"]) > 1:
-                print(f"FaceName: {face['faceName']}")
-            print(face["colorIdentity"])
-            print(face["text"])
-            print(face["type"])
-        print("-----------")
+                print(f"Name: {face['faceName']}")
+            else:
+                print(f'Name: {face["name"]}')
+            if not "Land" in face["type"]:
+                print(f'Cost: {face["manaCost"]}')
+                print("isLand: False")
+            else:
+                print(f'Cost: None')
+                print("isLand: True")
+            print(f'Rank: {face["edhrecRank"]}')
