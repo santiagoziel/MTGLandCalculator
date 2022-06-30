877f9a66835a30e2f392d77735e001ff62c2117c
santiagoziel <santiago_oziel92@hotmail.com>
2022-06-30 17:29:06 -0500

    retruve info db info base ond txt entry

--- utils/getColors.py
+++ utils/getColors.py
@@ -1,4 +1,4 @@
-import requests, re, time
+import requests, re, time,redis
 
 def checkColors(name):
     r = requests.get(f"https://api.scryfall.com/cards/named?exact={name}")
@@ -16,27 +16,34 @@ W = 0
 U = 0
 B = 0
 L = 0
-with open("decks\Deck - Obunn upgrades.txt", "r") as txt_file:
+r = redis.Redis(host="localhost", port=6379, db=0)
+with open("../decks/Deck - redis deck test.txt", "r") as txt_file:
     file_content = txt_file.read()
     content_list = file_content.split("\n")
+    #mtggoldfish adds two blank lines at the end of deck files for some reason
     content_list = content_list [:-2]
-    for x in content_list:
-        for card in range(int(x[:2])):
-            time.sleep(.11)
-            symbols = checkColors(f"{x[2:]}")
-            R += symbols.count("R")
-            G += symbols.count("G")
-            W += symbols.count("W")
-            U += symbols.count("U")
-            B += symbols.count("B")
-            L += symbols.count("L")
-
-sum = R+G+W+U+B
-print(f"R{R} G{G} W{W} U{U} B{B} sum{sum}")
-
-print("Tierras:")
-print(f"R: {(R*L)/sum}")
-print(f"G: {(G*L)/sum}")
-print(f"W: {(W*L)/sum}")
-print(f"U: {(U*L)/sum}")
-print(f"B: {(B*L)/sum}")
+    for entry in content_list:
+        amount, name = entry.split(" ",1)
+        print(f"amoutn: {amount}, card {name}")
+        info = r.hgetall(name)
+        print(f"info: {info}")
+        # for card in range(int(x[:2])):
+        #     print(card)
+#             time.sleep(.11)
+#             symbols = checkColors(f"{x[2:]}")
+#             R += symbols.count("R")
+#             G += symbols.count("G")
+#             W += symbols.count("W")
+#             U += symbols.count("U")
+#             B += symbols.count("B")
+#             L += symbols.count("L")
+#
+# sum = R+G+W+U+B
+# print(f"R{R} G{G} W{W} U{U} B{B} sum{sum}")
+#
+# print("Tierras:")
+# print(f"R: {(R*L)/sum}")
+# print(f"G: {(G*L)/sum}")
+# print(f"W: {(W*L)/sum}")
+# print(f"U: {(U*L)/sum}")
+# print(f"B: {(B*L)/sum}")
