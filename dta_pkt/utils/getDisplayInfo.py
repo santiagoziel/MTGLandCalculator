0b48c23e01f1dcab03e3d8c0d73254e0fbcd442e
santiagoziel <santiago_oziel92@hotmail.com>
2022-07-03 19:34:03 -0500

    display info now stored in object instead of array

--- dta_pkt/utils/getDisplayInfo.py
+++ dta_pkt/utils/getDisplayInfo.py
@@ -1,6 +1,14 @@
 import requests, json
 
 #lands = [('Blightstep Pathway', 8), ('Clearwater Pathway', 8), ("Xander's Lounge", 8), ('Riverglide Pathway', 6), ('Haunted Ridge', 4)]
+class Land:
+    def __init__(self, name, age, image):
+        self.name = name
+        self.price = age
+        self.image = image
+    def __repr__(self):
+        return f"Land(Name: {self.name}, Price: {self.price})"
+
 
 def gen_display_info(lands):
     identifiers = [{"name": name[0]} for name in lands]
@@ -10,10 +18,9 @@ def gen_display_info(lands):
     data = json.loads(reponse.text)
     displayinfo = []
     for card in data['data']:
-        displayCard =[card["name"],card["prices"]['usd']]
         if "card_faces" in card:
-            displayCard.append(card["card_faces"][0]["image_uris"]["normal"])
+            displayCard = Land(card["name"], card["prices"]['usd'], card["card_faces"][0]["image_uris"]["normal"])
         else:
-            displayCard.append(card["image_uris"]["normal"])
+            displayCard = Land(card["name"], card["prices"]['usd'], card["image_uris"]["normal"])
         displayinfo.append(displayCard)
     return displayinfo
